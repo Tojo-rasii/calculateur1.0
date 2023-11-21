dc2c03a334e1dd655c695437df1bcd8bbc512a79
Tojo-rasii <tojorasii1619@gmail.com>
2023-11-22 00:18:55 +0300

    success

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const valider = document.getElementById("icon-size");
+const load = document.getElementById("img-size");
+
 document.getElementById("calculer").addEventListener('click', function () {
     const name = document.getElementById('name').value;
     const budget = parseFloat(document.getElementById('budget').value);
@@ -12,19 +15,31 @@ document.getElementById("calculer").addEventListener('click', function () {
     const T3 = T2 * jour;
     const reste = budget - T3;
 
-    if (reste >= 0) {
-        document.querySelector('h3').style.color = "green";
-        document.getElementById("resultat").innerText += `Bonjour ${name}\n`
-        document.getElementById("resultat").innerText += `Budget ${budget} Ar \n`
-        document.getElementById("resultat").innerText += `Nombre de personne ${personne} \n`
-        document.getElementById("resultat").innerText += `Total de votre voyage ${T3} \n`
-        document.getElementById("resultat").innerText += `Reste de votre budget ${reste} \n`
-    }
-    else {
-        document.querySelector('h3').style.color = "red";
-        document.getElementById("resultat").innerText += `Bonjour ${name}\n`
-        document.getElementById("resultat").innerText += `Budget ${budget} Ar \n`
-        document.getElementById("resultat").innerText += `Attention votre budget est depasse ${reste}\n`
-    }
+    setTimeout(()=>{
+        if (reste >= 0) {
+            document.querySelector('h3').style.color = "green";
+            document.getElementById("resultat").innerText += `Bonjour ${name}\n`
+            document.getElementById("resultat").innerText += `Budget ${budget} Ar \n`
+            document.getElementById("resultat").innerText += `Nombre de personne ${personne} \n`
+            document.getElementById("resultat").innerText += `Total de votre voyage ${T3} \n`
+            document.getElementById("resultat").innerText += `Reste de votre budget ${reste} \n`
+            load.style.display = "none";
+            valider.style.display = "block";
+        }
+        else {
+            document.querySelector('h3').style.color = "red";
+            document.getElementById("resultat").innerText += `Bonjour ${name}\n`
+            document.getElementById("resultat").innerText += `Budget ${budget} Ar \n`
+            document.getElementById("resultat").innerText += `Attention votre budget est depasse ${reste}\n`
+            load.style.display = "none";
+            valider.style.display = "block";
+        }
+    },1000)
+
+   setTimeout(()=>{
+     load.style.display = "block";
+    valider.style.display = "none";
+   },3)
+   
 
 })
